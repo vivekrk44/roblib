697c243d1adb7de5fa02547c0c136f2dcb583273
vivekrk44 <vivekrk44@gmail.com>
2025-04-23 17:11:38 +0400

    Added simple_logger for easier print to console with colors for better debugging

---
+++ include/utils/logger/simple_logger.hpp
@@ -0,0 +1,59 @@
+#pragma once
+
+#include <iostream>
+#include <iomanip>
+#include <unordered_map>
+class SimpleLogger
+{
+
+public: 
+  /**
+   * @enum Color
+   * @brief Terminal colors for logging messages
+   */
+  enum class Color
+  {
+      RED,
+      GREEN,
+      YELLOW,
+      BLUE,
+      MAGENTA,
+      CYAN,
+      WHITE,
+      RESET
+  };
+  
+  /**
+   * @brief Log function for debugging with different colors
+   * @param color Color of the message
+   */
+  void print(Color color = Color::WHITE)
+  {
+    std::cout << getColorCode(color) << _ss.str() << getColorCode(Color::RESET) << std::endl; 
+    _ss.str("");
+  }
+
+  std::stringstream _ss;
+private:
+  /**
+   * @brief Get the ANSI color code for the specified color
+   * @param color The color enum value
+   * @return The ANSI escape sequence for the color
+   */
+  std::string getColorCode(Color color) const
+  {
+      static const std::unordered_map<Color, std::string> colorCodes = {
+          {Color::RED,     "\033[1;31m"},
+          {Color::GREEN,   "\033[1;32m"},
+          {Color::YELLOW,  "\033[1;33m"},
+          {Color::BLUE,    "\033[1;34m"},
+          {Color::MAGENTA, "\033[1;35m"},
+          {Color::CYAN,    "\033[1;36m"},
+          {Color::WHITE,   "\033[1;37m"},
+          {Color::RESET,   "\033[0m"}
+      };
+
+      auto it = colorCodes.find(color);
+      return (it != colorCodes.end()) ? it->second : colorCodes.at(Color::RESET);
+  }
+};
